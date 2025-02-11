1ec17d1488d2c6043e67f2b9e0d08019465cf50a
jisu <dlawotn3@naver.com>
2025-02-11 11:50:56 +0900

    style: change snake to camel

--- app/api/user.py
+++ app/api/user.py
@@ -20,12 +20,12 @@ def create_user_api(user: UserCreate):
     return created_user
 
 @router.get(
-    "/{user_id}",
+    "/{userId}",
     response_model=UserResponse,
     summary="사용자 정보 조회 API"
 )
-def get_user_api(user_id: int):
-    user = get_user_by_id(user_id)
+def get_user_api(userId: int):
+    user = get_user_by_id(userId)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
