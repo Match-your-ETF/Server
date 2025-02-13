cd0e7123a61d0a9efecaadff1a46360bf0c7b02d
jisu <dlawotn3@naver.com>
2025-02-13 14:03:01 +0900

    chore: add middleware for cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from app.api.user import router as user_router
 from app.api.etf import router as etf_router
 from app.api.mbti import router as mbti_router
 from app.api.market_indicator import router as market_indicator_router
 from app.api.portfolio import router as portfolio_router
+
+load_dotenv()
+API_URL = os.getenv("API_URL")
+WEB_URL = os.getenv("WEB_URL")
 
 app = FastAPI(
     title="Match your ETF Server API",
@@ -11,6 +18,20 @@ app = FastAPI(
     version="1.0.0",
     docs_url="/docs",
     redoc_url="/redoc"
+)
+
+origins = [
+    "http://localhost:5173",
+    WEB_URL,
+    API_URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # 라우터 등록
