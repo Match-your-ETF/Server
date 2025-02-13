3d133eb639cbbc1e287b98da55a1e58eb5d11dfd
jisu <dlawotn3@naver.com>
2025-02-13 15:20:53 +0900

    hotfix: fix response dto

--- app/schemas/market_indicator.py
+++ app/schemas/market_indicator.py
@@ -2,7 +2,7 @@ from typing import List
 from pydantic import BaseModel
 from datetime import datetime
 
-class MarketIndicator(BaseModel):
+class MarketIndicatorResponse(BaseModel):
     market_indicator_id: int
     name: str
     interest_rate: float
@@ -11,8 +11,5 @@ class MarketIndicator(BaseModel):
     created_at: datetime
     updated_at: datetime
 
-class MarketIndicatorResponse(BaseModel):
-    MarketIndicator: MarketIndicator
-
 class MarketIndicatorsResponse(BaseModel):
-    data: List[MarketIndicator]
+    data: List[MarketIndicatorResponse]
